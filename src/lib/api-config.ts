c669fcbd87861c9dba582fb4a3ba18abf08b2444
Dollar Mr <mrdollarhere@gmail.com>
2026-03-19 09:17:15 +0000

    update the link for me please:"https://script.google.com/macros/s/AKfycb

--- src/lib/api-config.ts
+++ src/lib/api-config.ts
@@ -1,4 +1,4 @@
 /**
  * Global configuration for the QuestFlow API.
  */
-export const API_URL = "https://script.google.com/macros/s/AKfycbwzxEE5S4megOGaJyZDNKUBBgJyxku9LVJ0E9_RjWoIyCdsu_bBquyEcSNTPZMCtfI4ZA/exec";
+export const API_URL = "https://script.google.com/macros/s/AKfycbyyn0DOfm-unH4fISASuV7-gj8-pcPWMcot2kD89GTTdKZAy67BrnSIagbc-Zndgzc2ng/exec";
