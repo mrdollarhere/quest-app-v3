c702341ccd357701036024492b11883f9a1b81d0
Dollar Mr <mrdollarhere@gmail.com>
2026-03-19 03:08:04 +0000

    I GOT THE LINK AFTER DEPLOY HELP ME UPDATE THE LNK:""https://script.goog

--- src/lib/api-config.ts
+++ src/lib/api-config.ts
@@ -1,4 +1,4 @@
 /**
  * Global configuration for the QuestFlow API.
  */
-export const API_URL = "https://script.google.com/macros/s/AKfycbxD0P_i2bmNpGH3QPJEsq7cGRac-EFtzo25glrQ10GPoARAyg_Vf4DmAqe0WBf6hw1VjQ/exec";
+export const API_URL = "https://script.google.com/macros/s/AKfycbync2S3cI6ywlq-MCtiuL7nyK1SiEARToCVfvAdY7Ya30qRAoT2f2M4naG2wgCB5V_2JQ/exec";
