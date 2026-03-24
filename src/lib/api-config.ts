c083f53d6f3a5e83c838c74e9a7dda6682503c80
Dollar Mr <mrdollarhere@gmail.com>
2026-03-24 12:49:42 +0000

    here the new url after update gs de:https://script.google.com/macros/s/A

--- src/lib/api-config.ts
+++ src/lib/api-config.ts
@@ -2,4 +2,4 @@
  * Global configuration for the QuestFlow API.
  * Updated to the latest deployment for high-speed synchronization.
  */
-export const API_URL = "https://script.google.com/macros/s/AKfycbwVfKTAH5eUHhhLVZyLyZk0AbiLr6zwloMR-aYlssVK35JY40cShJ4vnxkOT3EgAvRRFQ/exec";
+export const API_URL = "https://script.google.com/macros/s/AKfycbzj9Xmqeu_LWGXje6r_ha3Yf4Q7XWlGZh3r5I8TiOuLB82dy9uOOY-AkMyWEiLO2bvP6g/exec";
