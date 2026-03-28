306437cca27da6296b800b9aa8eae72501b0759e
Dollar Mr <mrdollarhere@gmail.com>
2026-03-28 11:01:50 +0000

    here te url for the gs v18 please update it in the config:
    https://scri

--- src/lib/api-config.ts
+++ src/lib/api-config.ts
@@ -2,4 +2,4 @@
  * Global configuration for the QuestFlow API.
  * Updated to the latest deployment for high-speed synchronization.
  */
-export const API_URL = "https://script.google.com/macros/s/AKfycbwKZ1Hq7JzXyabMMbGHqegwjS_AA4ps_bqsZZZpMK-ajE1JjPqVwyb43HjtmwyY471rgA/exec";
+export const API_URL = "https://script.google.com/macros/s/AKfycbwLfIMbW_ef1ZvPPJmJAc68uIYEydZkTSmcFB7qTOCeUVDx9QfI-0GtcEtBtIo2Z8JKGQ/exec";
